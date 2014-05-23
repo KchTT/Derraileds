3ca683b200b747919de48d9a472d5aca7a82a66a
Martin Bronzino <martin@dlab.com.ar>
2014-05-23 18:25:00 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1">
+<title>Accidentes</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!--[if lt IE 9]>
+<script>
+   document.createElement("nav");
+   document.createElement("header");
+   document.createElement("footer");
+   document.createElement("section");
+   document.createElement("article");
+   document.createElement("aside");
+   document.createElement("hgroup");
+</script>
+<script src="http://HTML5shim.googlecode.com/svn/trunk/HTML5.js"></script>
+<![endif]-->
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  body{
+    background-color: #000;
+  }
+</style>
+</head>
+
+<body>
+
+
+</body>
+</html>  
